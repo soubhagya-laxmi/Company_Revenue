15eaf9f1a58db40ae158bb018b383e99b10a08c2
soubhagya Laxmi tripathy <shubbi.tripathy@gmail.com>
2022-08-16 19:15:56 +0530

    Company yahoo ticker

--- Yahoo_Ticker.R
+++ Yahoo_Ticker.R
@@ -1,37 +1,14 @@
 
-
-
-url <- "https://en.wikipedia.org/wiki/List_of_companies_listed_on_the_National_Stock_Exchange_of_India"
-webpage <- read_html(url)
-tbls <- html_nodes(webpage, "table")
-tbls_ls <- webpage %>%
-  html_nodes("table") %>%
-  .[[28]] %>%
-  html_table(fill = TRUE)
-View(tbls_ls)
-sheet_append(drive_get("sample"),tbls_ls)
-
-############################################################################
-
-Sheet1 <- read_sheet(drive_get("sample"))
-
-data <- data.frame()
-for(x in 1001:1809)
-{
-  Search <- Sheet1$NSE[x]
-  #Extract string after (:)
-  Ind <- sub('.*:', '', Search)
-  Ind <- sub('.', '',Ind)
-  ticker <- paste(Ind,".NS")
-  ticker <- str_replace_all(string=ticker, pattern=" ", repl="")
-  new <- data.frame(Company = Sheet1$Company[x],Country = "🇮🇳 India",
-                    Ticker = ticker)
-  data <- rbind(data,new)
-}
-View(data)
-
-sheet_append(drive_get("yahoo_Ticker"),data)
-==============================================================================
+library(httr)
+library(XML)
+library(xml2)
+library(rvest)
+library(readr)
+library(htm2txt) 
+library(googledrive)
+library(googlesheets4)
+library(stringr)
+library(RCurl)
 
 Sheet1 <- read_sheet(drive_get("sample"))
 
